644000a820725b9e2ad529f856bf435653386a67
Takhirchik <tokha.froma@mail.ru>
2025-02-25 12:24:34 +0400

    Добавил скрипт SpringBootApplication

---
+++ src/main/java/com/warlock/Main.java
@@ -0,0 +1,13 @@
+package com.warlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtinctsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtinctsWebApplication.class, args);
+    }
+
+}
