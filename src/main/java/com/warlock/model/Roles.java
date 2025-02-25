07fb4a72296ebe1eab783455e67d08f6d5c56d51
Takhirchik <tokha.froma@mail.ru>
2025-02-25 13:03:30 +0400

    Добавил сущность Role

---
+++ src/main/java/com/warlock/model/Roles.java
@@ -0,0 +1,52 @@
+package com.warlock.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+//import javax.persistence.NamedQueries;
+//import javax.persistence.NamedQuery;
+
+
+@Entity
+@Table(name="roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name", nullable=false, unique=true)
+    private String name;
+
+    public Role(){
+    }
+
+    public Role(String name){
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString(){
+        return "Role{" +
+                "id=" + id +
+                ", name=" + name +
+                "}";
+    }
+}
