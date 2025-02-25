883b1546bd3d94a6a1026bda6bf5749984866cc4
Takhirchik <tokha.froma@mail.ru>
2025-02-25 13:12:48 +0400

    Добавил JpaRepository, убрал все не нужные методы

--- src/main/java/com/warlock/repo/UserRepo.java
+++ src/main/java/com/warlock/repo/UserRepo.java
@@ -1,36 +1,7 @@
 package com.warlock.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 import com.warlock.model.User;
 
-@Repository
-public interface UserRepo extends CrudRepository<User, Long> {
-
-    Optional<User> findByNickname(String nickname);
-
-    Optional<User> findByLogin(String login);
-
-    Optional<User> findByEmail(String email);
-
-    Iterable<User> findAllByNickname(Iterable<String> nicknames);
-
-    Iterable<User> findAllByLogin(Iterable<String> logins);
-
-    Iterable<User> findAllByEmail(Iterable<String> email);
-
-    void deleteByLogin(String login);
-
-    void deleteByEmail(String email);
-
-    void deleteAllByLogin(Iterable<String> logins);
-
-    void deleteAllByEmail(Iterable<String> emails);
-
-    void editUser(User user);
-
-    void editAllUser(Iterable<User> user);
-
-}
+public interface UserRepo extends JpaRepository<User, Long> {}
