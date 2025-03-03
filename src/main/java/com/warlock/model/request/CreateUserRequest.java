08b308a636455f7f903fbec1d56458880868f6a0
Takhirchik <tokha.froma@mail.ru>
2025-03-03 15:02:21 +0400

    Исправил модели CreateUserRequest и RoleResponse

--- src/main/java/com/warlock/model/request/CreateUserRequest.java
+++ src/main/java/com/warlock/model/request/CreateUserRequest.java
@@ -11,5 +11,5 @@ public class CreateUserRequest {
     private String login;
     private String password;
     private String email;
-    private CreateRoleRequest Role;
+    private CreateRoleRequest role;
 }
--- src/main/java/com/warlock/model/response/RoleResponse.java
+++ src/main/java/com/warlock/model/response/RoleResponse.java
@@ -6,6 +6,6 @@ import lombok.experimental.Accessors;
 @Data
 @Accessors(chain = true)
 public class RoleResponse {
-//    private Long id;
+    private Long id;
     private String name;
 }
