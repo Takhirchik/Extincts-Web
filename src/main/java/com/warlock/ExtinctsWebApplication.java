5f830f88672b0334af069fb02a5d623b6996cf91
Takhirchik <tokha.froma@mail.ru>
2025-03-03 18:16:16 +0400

    Добавил поисковые аннотации

--- src/main/java/com/warlock/ExtinctsWebApplication.java
+++ src/main/java/com/warlock/ExtinctsWebApplication.java
@@ -2,8 +2,12 @@ package com.warlock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.warlock.domain")
+@EnableJpaRepositories("com.warlock.repository")
 public class ExtinctsWebApplication {
 
     public static void main(String[] args) {
