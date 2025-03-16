5b99147356623b5ca653e7f88a28bc6cac1f8b56
Takhirchik <tokha.froma@mail.ru>
2025-03-16 21:28:44 +0400

    Ввёл профили prod и test. Добавил скрипты Liquibase в prod и test. Обновил docker-compose теперь работает без start.sh. Обновил колонку email в сущности User, добавив ей уникальность

--- src/main/java/com/warlock/domain/User.java
+++ src/main/java/com/warlock/domain/User.java
@@ -23,7 +23,7 @@ public class User {
     private String login;
     @Column(name = "password", nullable = false)
     private String password;
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @ManyToOne
